f15488bde20cde3e3aa484ca72ba2675ffd720af
I.Efimov <i.efimov.jamteam@gmail.com>
2021-04-24 18:33:00 +0600

    test task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
+import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { AppBar, Drawer, Typography, MenuList, MenuItem } from '@material-ui/core';
+import {menus, content} from './const';
+import { ExitToApp, HelpOutline} from '@material-ui/icons';
 
 function App() {
+
+  const [activeMenu, setActiveMenu] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Drawer variant="permanent" className="sidebar">
+        <div className="logo">
+          <img src={logo} alt=""/>
+        </div>
+        <div className="center-menu">
+          <MenuList>
+            {
+              menus.map((menu, key) => (
+                <MenuItem
+                  key={key}
+                  onClick={() => setActiveMenu(key)}
+                  selected={key === activeMenu}
+                  >
+                  {menu.icon}
+                </MenuItem>
+              ))
+            }
+          </MenuList>
+        </div>
+        <div className="bottom-menu">
+          <MenuList>
+            <MenuItem><HelpOutline/></MenuItem>
+            <MenuItem><ExitToApp/></MenuItem>
+          </MenuList>
+        </div>
+      </Drawer>
+      <main className="main-content">
+        <AppBar className="header" color="transparent">
+          <Typography variant="h4">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</Typography>
+        </AppBar>
+        <div className="content">
+          {content[activeMenu]}
+        </div>
+      </main>
     </div>
   );
 }
